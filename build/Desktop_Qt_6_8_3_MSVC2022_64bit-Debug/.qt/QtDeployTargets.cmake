bdfb50a1565d40dbd6d822b797ef27c8a91f1e3e
sirius2beta <sirius2beta@gmail.com>
2026-02-04 00:26:46 +0800

    delete build

--- build/Desktop_Qt_6_8_3_MSVC2022_64bit-Debug/.qt/QtDeployTargets.cmake
+++
@@ -1,3 +0,0 @@
-set(__QT_DEPLOY_TARGET_LinkPackage_FILE C:/project/LinkPackage/build/Desktop_Qt_6_8_3_MSVC2022_64bit-Debug/LinkPackage.exe)
-set(__QT_DEPLOY_TARGET_LinkPackage_TYPE EXECUTABLE)
-set(__QT_DEPLOY_TARGET_LinkPackage_RUNTIME_DLLS C:/Qt/6.8.3/msvc2022_64/bin/Qt6SerialPortd.dll;C:/Qt/6.8.3/msvc2022_64/bin/Qt6Networkd.dll;C:/Qt/6.8.3/msvc2022_64/bin/Qt6Cored.dll)
